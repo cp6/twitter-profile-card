94dae03f27c8d1e2075c3c1602ccdd066d56ccba
corbpie <cp6@users.noreply.github.com>
2023-04-26 23:46:07 +1000

    Cleanup and optimise
    Cleanup and optimise

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
-<?PHP
-error_reporting(0);
-ini_set('display_errors', 0);
-function buildBaseString($baseURI, $method, $params)
+<?php
+
+function buildBaseString(string $baseURI, string $method, array $params): string
 {
     $r = array();
     ksort($params);
@@ -10,50 +9,60 @@ function buildBaseString($baseURI, $method, $params)
     }
     return $method . "&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
 }
-function buildAuthorizationHeader($oauth)
+
+function buildAuthorizationHeader($oauth): string
 {
     $r = 'Authorization: OAuth ';
     $values = array();
-    foreach ($oauth as $key => $value)
+    foreach ($oauth as $key => $value) {
         $values[] = "$key=\"" . rawurlencode($value) . "\"";
+    }
     $r .= implode(', ', $values);
     return $r;
 }
-function returndata($user)
+
+function returndata(string $user): array
 {
     $oauth_access_token = "XXX";
     $oauth_access_token_secret = "XXX";
     $consumer_key = "XXX";
     $consumer_secret = "XXX";
     $twitter_timeline = "user_timeline";
-    $request = array(
+
+    $request = [
         'screen_name' => $user,
         'count' => '3'
-    );
-    $oauth = array(
+    ];
+
+    $oauth = [
         'oauth_consumer_key' => $consumer_key,
         'oauth_nonce' => time(),
         'oauth_signature_method' => 'HMAC-SHA1',
         'oauth_token' => $oauth_access_token,
         'oauth_timestamp' => time(),
         'oauth_version' => '1.0'
-    );
+    ];
+
     $oauth = array_merge($oauth, $request);
     $base_info = buildBaseString("https://api.twitter.com/1.1/statuses/$twitter_timeline.json", 'GET', $oauth);
     $composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret);
     $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
     $oauth['oauth_signature'] = $oauth_signature;
-    $header = array(buildAuthorizationHeader($oauth), 'Expect:');
-    $options = array(CURLOPT_HTTPHEADER => $header,
+    $header = [buildAuthorizationHeader($oauth), 'Expect:'];
+    $options = [
+        CURLOPT_HTTPHEADER => $header,
         CURLOPT_HEADER => false,
         CURLOPT_URL => "https://api.twitter.com/1.1/statuses/$twitter_timeline.json?" . http_build_query($request),
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_SSL_VERIFYPEER => false);
+        CURLOPT_SSL_VERIFYPEER => false
+    ];
     $feed = curl_init();
     curl_setopt_array($feed, $options);
     $json = curl_exec($feed);
     curl_close($feed);
+
     $tweets = json_decode($json, true);
+
     foreach ($tweets as $tweet) {
         $userid = $tweet["user"]["id"];
         $name = $tweet["user"]["name"];
@@ -71,13 +80,26 @@ function returndata($user)
         $cover_img = $tweet["user"]["profile_background_image_url_https"];
         $profile_banner = $tweet["user"]["profile_banner_url"];
         $profile_img = $tweet["user"]["profile_image_url_https"];
-        $profile_img = str_replace("_normal","","$profile_img");
+        $profile_img = str_replace("_normal", "", $profile_img);
         $verified = $tweet["user"]["verified"];
-        if (is_null($website = $tweet["user"]["url"])){$website = NULL;}else{$website = $tweet["user"]["url"];};
-        if (is_null($tweet["user"]["entities"]["url"]['urls'][0]['expanded_url'])){$website_link = NULL;}else{$website_link = $tweet["user"]["entities"]["url"]['urls'][0]['expanded_url'];};
+
+        if (is_null($website = $tweet["user"]["url"])) {
+            $website = null;
+        }
+
+        if (is_null($tweet["user"]["entities"]["url"]['urls'][0]['expanded_url'])) {
+            $website_link = null;
+        } else {
+            $website_link = $tweet["user"]["entities"]["url"]['urls'][0]['expanded_url'];
+        }
+
     }
-    return array('id' => $userid, 'userName' => $username, 'name' => $name, 'location' => $location, 'description' => $description, 'follower' => $followers,
+
+    return [
+        'id' => $userid, 'userName' => $username, 'name' => $name, 'location' => $location, 'description' => $description, 'follower' => $followers,
         'following' => $following, 'created' => $made, 'favortitesCount' => $favourites_count, 'listedCount' => $listed_count, 'utcOffset' => $utc_offset,
         'timeZone' => $time_zone, 'tweets' => $tweets_amount, 'coverImage' => $cover_img, 'profileBanner' => $profile_banner, 'profileAvatar' => $profile_img,
-         'website' => $website, 'websiteLink' => $website_link, 'verified' => $verified);
+        'website' => $website, 'websiteLink' => $website_link, 'verified' => $verified
+    ];
+
 }
